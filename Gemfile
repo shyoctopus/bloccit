ea013f9b837d17d2718da6e3bc466896f48453f0
Michelle Cardinal <michelle.cardinal@yahoo.com>
2015-09-25 12:14:17 -0700

    39.Checkpoint

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ end
 
 gem 'bootstrap-sass'
 
+gem 'bcrypt'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924195018) do
+ActiveRecord::Schema.define(version: 20150925185612) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20150924195018) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
