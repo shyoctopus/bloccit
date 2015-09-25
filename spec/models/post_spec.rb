0b2ad724baf2c180c77cabeceacd9b8a5ff8601c
Michelle Cardinal <michelle.cardinal@yahoo.com>
2015-09-25 11:48:28 -0700

    38.Checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,16 +4,22 @@ include RandomData
 RSpec.describe Post, type: :model do
 ##  pending "add some examples to (or delete) #{__FILE__}"
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   it { should belong_to(:topic) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
    it "should respond to title" do
     expect(post).to respond_to(:title)
   end
-  
+
   it "should respond to body" do
     expect(post).to respond_to(:body)
   end
